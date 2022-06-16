d5761f8c4f0069621f23c70a3768f699a87024f2
IrinaIaniv <yanividm@gmail.com>
2022-06-16 15:06:32 +0300

    add index and array

---
+++ Example_011/Program.cs
@@ -0,0 +1,29 @@
+﻿void FillArray(int[] collection)
+{
+    int length = collection.Length;
+    int index = 0;
+
+    while (index < length)
+    {
+        collection[index] = new Random().Next(80, 156);
+        index++;
+    } 
+
+}
+
+void PrintArray(int[] coll)
+{
+     int count = coll.Length;
+     int possition = 0;
+     while (possition < count)
+     {
+        Console.WriteLine(coll[possition]);
+        possition++;
+     }
+
+}
+
+int[] array = new int[5];
+
+FillArray(array);
+PrintArray(array);
