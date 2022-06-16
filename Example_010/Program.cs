8d1749690077e8fb44fb490c2149180a3d21405f
IrinaIaniv <yanividm@gmail.com>
2022-06-16 14:17:53 +0300

    add array and find index

---
+++ Example_010/Program.cs
@@ -0,0 +1,20 @@
+﻿int[]array = {8, 6, 4, 3, 2, 4, 0, 3 };
+
+int n = array.Length;
+
+int find = 4;
+int index = 0;
+while (index < n)
+{
+    if(array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    
+    
+     index = index + 1;
+
+
+}
+
